7e73389e154c3589d85e78045376a4bf80386ca2
AryaN1k <aryanvats1028@gmail.com>
2024-09-01 18:36:32 +0530

    first commit

--- src/Components/Testo.jsx
+++ src/Components/Testo.jsx
@@ -2,14 +2,16 @@ import { FaStar } from "react-icons/fa";
 function Testo() {
   return (
     <section className="min-w-screen min-h-full flex flex-col items-center justify-evenly mb-8">
-      <div className="h-56 w-full md:w-1/2 md:mx-auto grid items-start md:items-center md:justify-items-center py-7 mb-7">
-        <h1 className="text-4xl md:text-6xl font-semibold">Testimonials</h1>
-        <span className="font-medium">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, quos?
+      <div className="container h-56 w-full flex flex-col justify-center px-4">
+        <h1 className="text-5xl md:text-6xl font-semibold py-4">Testimonials</h1>
+        <span className="font-medium border">
+          Lorem ipsum dolor sit amet 
+          <br />
+          adipisicing elit. Eaque, quos?
         </span>
       </div>
-      <main className="container border mx-auto grid md:grid-cols-3 gap-8 justify-items-center py-8">
-        <div className="bg-white rounded-2xl p-6 w-3/4">
+      <main className="container border md:mx-auto grid md:grid-cols-3 gap-8 md:justify-items-center py-8 px-5">
+        <div className="bg-white rounded-2xl p-6 w-full md:w-3/4">
           <h4 className="text-center">
             Since implementing Lando our business has seen significant growth.
           </h4>
@@ -23,7 +25,7 @@ function Testo() {
           <h2 className="text-center font-medium text-lg">Uday Halwa</h2>
           <p className="text-center">Saharnpur , Up</p>
         </div>
-        <div className="bg-white rounded-2xl p-6 w-3/4">
+        <div className="bg-white rounded-2xl w-full p-6 md:w-3/4">
           <h4 className="text-center">
             Since implementing Lando our business has seen significant growth.
           </h4>
@@ -37,7 +39,7 @@ function Testo() {
           <h2 className="text-center font-medium text-lg">Uday Halwa</h2>
           <p className="text-center">Saharnpur , Up</p>
         </div>
-        <div className="bg-white rounded-2xl p-6 w-3/4">
+        <div className="bg-white rounded-2xl w-full p-6 md:w-3/4">
           <h4 className="text-center">
             Since implementing Lando our business has seen significant growth.
           </h4>
